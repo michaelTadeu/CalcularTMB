8df6def9d275a39fd1a1b424867f71aafc0970bb
Michael Tadeu Alves de Oliveira <michael89.oliveira@gmail.com>
2018-07-21 15:29:49 -0300

    Commit projeto inteiro.

---
+++ Calcular/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Calcular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtResultadoTMB = new System.Windows.Forms.TextBox();
+            this.txtResultadoGET = new System.Windows.Forms.TextBox();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Idade:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sexo:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Peso:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado TMB:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado GET:";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(129, 46);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 5;
+            this.txtIdade.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(129, 78);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(100, 20);
+            this.txtSexo.TabIndex = 6;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(129, 109);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 7;
+            // 
+            // txtResultadoTMB
+            // 
+            this.txtResultadoTMB.Location = new System.Drawing.Point(129, 162);
+            this.txtResultadoTMB.Name = "txtResultadoTMB";
+            this.txtResultadoTMB.Size = new System.Drawing.Size(100, 20);
+            this.txtResultadoTMB.TabIndex = 8;
+            // 
+            // txtResultadoGET
+            // 
+            this.txtResultadoGET.Location = new System.Drawing.Point(129, 192);
+            this.txtResultadoGET.Name = "txtResultadoGET";
+            this.txtResultadoGET.Size = new System.Drawing.Size(100, 20);
+            this.txtResultadoGET.TabIndex = 9;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Location = new System.Drawing.Point(75, 235);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(86, 23);
+            this.btCalcular.TabIndex = 10;
+            this.btCalcular.Text = "Calcular";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtResultadoGET);
+            this.Controls.Add(this.txtResultadoTMB);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtResultadoTMB;
+        private System.Windows.Forms.TextBox txtResultadoGET;
+        private System.Windows.Forms.Button btCalcular;
+    }
+}
+
---
+++ Calcular/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = Convert.ToInt32(txtIdade.Text);
+            string sexo = txtSexo.Text;
+            double peso = Convert.ToDouble(txtPeso.Text);
+
+            int valor = 239;
+
+            //CONSTANTES DE VALORES FEMININOS
+            double valorFeminino1 = 0.047;
+            double valorFeminino2 = 2.951;
+            double valorFeminino3 = 0.048;
+            double valorFeminino4 = 2.562;
+
+            //CONSTANTES DE VALORES MASCULINOS
+            double valorMasculino1 = 0.084;
+            double valorMasculino2 = 2.122;
+            double valorMasculino3 = 0.056;
+            double valorMasculino4 = 2.800;
+
+            double resultadoTMB = 0;
+            double resultadoGET = 0;
+
+            if (sexo == "F" || sexo == "f")
+            {
+                if (idade >= 12 && idade <= 18) 
+                {
+                    resultadoTMB = (((valorFeminino1 * peso) + valorFeminino2) * valor);
+                    txtResultadoTMB.Text = String.Format("{0:N}", resultadoTMB);
+                } else
+                {
+                    if (idade == 19)
+                    {
+                        resultadoTMB = (((valorFeminino3 * peso) + valorFeminino4) * valor);
+                        txtResultadoTMB.Text = String.Format("{0:N}", resultadoTMB);
+                    }
+                }
+            }
+
+            if (sexo == "M" || sexo == "m")
+            {
+                if (idade >= 12 && idade <= 18)
+                {
+                    resultadoTMB = (((valorMasculino1 * peso) + valorMasculino2) * valor);
+                    txtResultadoTMB.Text = String.Format("{0:N}", resultadoTMB);
+                }
+                else
+                {
+                    if (idade == 19)
+                    {
+                        resultadoTMB = (((valorMasculino3 * peso) + valorMasculino4) * valor);
+                        txtResultadoTMB.Text = String.Format("{0:N}", resultadoTMB);
+                    }
+                }
+            }
+        }
+    }
+}
